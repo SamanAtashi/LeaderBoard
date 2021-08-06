87a480731617597bebfd8c4b97af5ade51f8a448
SamanAtashi <milady00126@gmail.com>
2021-08-06 12:40:16 +0300

    Add score functionality + submit btn

--- src/index.js
+++ src/index.js
@@ -60,3 +60,32 @@ refBtn.addEventListener('click', async (e) => {
   iterateList(listPromise);
 });
 
+// todo: ===========================================> add to score
+
+async function sendScore(userName, userScore) {
+  const currentId = await gameIdPromise;
+  const temp = await fetch(`${baseUrl}${currentId}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+  });
+  return temp;
+}
+
+function submitFunction() {
+  const getYourName = document.getElementById('userName').value;
+  const getYourScore = document.getElementById('userScore').value;
+  sendScore(getYourName, getYourScore);
+  document.getElementById('userName').value = '';
+  document.getElementById('userScore').value = '';
+}
+
+document.querySelector('#submitBtn').addEventListener('click', async (e) => {
+  e.preventDefault();
+  submitFunction();
+});
