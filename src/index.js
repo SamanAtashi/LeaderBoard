70f95b6dbdf9fe4dfe336310559b7cb80234e9f5
SamanAtashi <milady00126@gmail.com>
2021-08-06 06:45:33 +0300

    Send score functionality API

--- src/index.js
+++ src/index.js
@@ -21,3 +21,30 @@ async function grabId() {
   const gameId = temp.result.split(' ')[3];
   return gameId;
 }
+
+// todo: ===========================================> send a score + recieve
+
+function sendScore(arg) {
+  fetch(arg, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: 'Saman',
+      score: 111,
+    }),
+  });
+}
+
+async function giveScoreUrl() {
+  const temp = await grabId();
+  const urlToScores = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${temp}/scores/`;
+
+  sendScore(urlToScores);
+  showScore(urlToScores);
+
+  return urlToScores;
+}
+
+giveScoreUrl();
