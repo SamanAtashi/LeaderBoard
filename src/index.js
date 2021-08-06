49196a2d2a1e58d8b3e5c8ec3225225c4cbd59c9
SamanAtashi <milady00126@gmail.com>
2021-08-06 06:39:47 +0300

    Create game functionality using API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+const urlToCreateGame = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+// todo: ===========================================> create game
+const createGame = async () => {
+  const temp = await fetch(urlToCreateGame, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'Saman"s game ----->',
+    }),
+  });
+  return temp.json();
+};
+
+async function grabId() {
+  const temp = await createGame();
+  const gameId = temp.result.split(' ')[3];
+  return gameId;
+}
